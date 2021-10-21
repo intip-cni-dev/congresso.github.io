904e720beca095cb155d2f3c0c8a5f368bfe1063
Michel Lacerda <michellacerdasilva@gmail.com>
2021-10-21 14:16:41 -0300

    update

---
+++ assets/src/js/realizacao.js
@@ -0,0 +1,131 @@
+const realizacaoTemplate = () => `
+<div class="container">
+    <div class="row my-4 py-4">
+        <div class="col-12 col-md-6">
+            <h5 class="text-uppercase text-center fw-normal">
+                <p class="mb-4">CORREALIZAÇÃO</p>
+                <span class="d-block horizontal-divider mx-5"></span>
+            </h5>
+            <div class="row row-cols-2 row-cols-lg-2 align-items-center g-2 align-items-center align-items-center py-5">
+                <a href="http://www.portaldaindustria.com.br/iel/" class="realizadores-item">
+                    <img src="./assets/dist/images/patrocinadores/realizacao/IEL.png" alt="IEL">
+                </a>
+                <a href="http://www.portaldaindustria.com.br/sesi/" class="realizadores-item">
+                    <img src="./assets/dist/images/patrocinadores/realizacao/SESI_SENAI.png" alt="SESI e SENAI">
+                </a>
+            </div>
+        </div>
+        <div class="col-12 col-md-6">
+            <h5 class="text-uppercase text-center fw-normal">
+                <p class="mb-4">REALIZAÇÃO</p>
+                <span class="d-block horizontal-divider mx-5"></span>
+            </h5>
+            <div class="row row-cols-1 row-cols-lg-2 align-items-center g-2 align-items-center align-items-center py-5">
+                <a href="https://www.sebrae.com.br/" class="realizadores-item">
+                    <img src="./assets/dist/images/patrocinadores/realizacao/SEBRAE.png" alt="SEBRAE">
+                </a>
+                <a href="http://www.portaldaindustria.com.br/cni/" class="realizadores-item">
+                    <img src="./assets/dist/images/patrocinadores/realizacao/CNI.png" alt="CNI">
+                </a>
+            </div>
+        </div>
+    </div>
+    <div class="row my-4 py-4">
+        <div class="col-12 col-md-4">
+            <h5 class="text-uppercase text-center fw-normal">
+                <p class="mb-4">patrocinador ouro</p>
+                <span class="d-block horizontal-divider mx-5"></span>
+            </h5>
+            <div class="row row-cols-2 row-cols-lg-2 align-items-center g-2 align-items-center align-items-center py-5">
+                <a href="https://embrapii.org.br" class="realizadores-item">
+                    <img src="./assets/dist/images/patrocinadores/ouro/EMBRAPI.png" alt="EMBRAPI">
+                </a>
+                <a href="https://br.fi-group.com/" class="realizadores-item">
+                    <img src="./assets/dist/images/patrocinadores/ouro/FIGROUP.png" alt="FIGROUP">
+                </a>
+            </div>
+        </div>
+        <div class="col-12 col-md-4">
+            <h5 class="text-uppercase text-center fw-normal">
+                <p class="mb-4">patrocinador master</p>
+                <span class="d-block horizontal-divider mx-5"></span>
+            </h5>
+            <div class="row row-cols-1 row-cols-lg-1 align-items-center g-2 align-items-center align-items-center py-5">
+                <a href="https://www.roche.com.br/" class="realizadores-item">
+                    <img src="./assets/dist/images/patrocinadores/master/ROCHE.png" alt="ROCHE">
+                </a>
+            </div>
+        </div>
+        <div class="col-12 col-md-4">
+            <h5 class="text-uppercase text-center fw-normal">
+                <p class="mb-4">patrocinador diamante</p>
+                <span class="d-block horizontal-divider mx-5"></span>
+            </h5>
+            <div class="row row-cols-1 row-cols-lg-1 align-items-center g-2 align-items-center align-items-center py-5">
+
+                <a href="http://www.finep.gov.br/" class="realizadores-item">
+                    <img src="./assets/dist/images/patrocinadores/diamante/FINEP+MCTI+GF.png" alt="FINEP, MCTI e GF">
+                </a>
+            </div>
+        </div>
+    </div>
+    <div class="row my-4 py-4">
+        <div class="col-12 col-md-6">
+            <h5 class="text-uppercase text-center fw-normal">
+                <p class="mb-4">patrocinador prata</p>
+                <span class="d-block horizontal-divider mx-5"></span>
+            </h5>
+            <div class="row row-cols-2 row-cols-lg-3 align-items-center g-2 align-items-center align-items-center py-5">
+                <a href="https://www.cba.com.br/cba/" class="realizadores-item">
+                    <img src="./assets/dist/images/patrocinadores/prata/CBA.png" alt="CBA">
+                </a>
+                <a href="https://www.gm.com/" class="realizadores-item">
+                    <img src="./assets/dist/images/patrocinadores/prata/GM.png" alt="GM">
+                </a>
+                <a href="http://tuzzi.com.br/" class="realizadores-item">
+                    <img src="./assets/dist/images/patrocinadores/prata/TUZZI.png" alt="TUZZI">
+                </a>
+            </div>
+        </div>
+        <div class="col-12 col-md-6">
+            <h5 class="text-uppercase text-center fw-normal">
+                <p class="mb-4">patrocinador bronze</p>
+                <span class="d-block horizontal-divider mx-5"></span>
+            </h5>
+            <div class="row row-cols-1 row-cols-lg-2 align-items-center g-2 align-items-center align-items-center py-5">
+                <a href="https://www.braskem.com.br/" class="realizadores-item">
+                    <img src="./assets/dist/images/patrocinadores/bronze/BRASKEM.png" alt="BRASKEM">
+                </a>
+                <a href="https://www.suzano.com.br/" class="realizadores-item">
+                    <img src="./assets/dist/images/patrocinadores/bronze/SUZANO.png" alt="SUZANO">
+                </a>
+            </div>
+        </div>
+    </div>
+    <div class="row my-4 py-4">
+        <div class="col-12 col-md-12">
+            <h5 class="text-uppercase text-center fw-normal">
+                <p class="mb-4">patrocinador apoio</p>
+                <span class="d-block horizontal-divider mx-5"></span>
+            </h5>
+            <div class="row row-cols-2 row-cols-lg-4 align-items-center g-2 align-items-center align-items-center py-5">
+                <a href="https://www.basf.com/br/pt.html" class="realizadores-item">
+                    <img src="./assets/dist/images/patrocinadores/apoio/BASF.png" alt="BASF">
+                </a>
+                <a href="https://www.iadb.org/" class="realizadores-item">
+                    <img src="./assets/dist/images/patrocinadores/apoio/BID.png" alt="BID">
+                </a>
+                <a href="https://www.cgee.org.br/" class="realizadores-item">
+                    <img src="./assets/dist/images/patrocinadores/apoio/CGEE.png" alt="CGEE">
+                </a>
+                <a href="https://new.siemens.com/br" class="realizadores-item">
+                    <img src="./assets/dist/images/patrocinadores/apoio/SIEMENS.png" alt="SIEMENS.png">
+                </a>
+            </div>
+        </div>
+    </div>
+</div>
+`
+const $barraRealizacao = document.getElementById("barra-realizacao");
+
+$barraRealizacao && $barraRealizacao.insertAdjacentHTML('afterbegin', realizacaoTemplate())
